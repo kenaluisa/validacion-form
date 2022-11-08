633feb1a264a05feb658ad7f8f86a139948ff03f
kenaluisa <matzenbacherkena@gmail.com>
2022-11-08 16:01:54 -0300

    haciendo commit del 50% del curso

---
+++ js/nacimiento.js
@@ -0,0 +1,32 @@
+export function valida(input){
+    const tipoDeInput= input.dataset.tipo;
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input);
+
+    }
+}
+
+const validadores ={
+    nacimiento:(input) => validarNacimiento(input),
+
+};
+
+function validarNacimiento(input){
+    const fechaCliente=new Date(input.value);
+    let mensaje='';
+
+    if(!mayorEdad(fechaCliente)){
+        mensaje="debes tener al menos 18 años de edad";
+    }
+
+    input.setCustomValidity(mensaje);
+};
+
+function mayorEdad(fecha){
+    const fechaActual=new Date();
+    const diferenciaFechas =new Date(fecha.getUTCFullYear() + 18,
+     fecha.getUTCMonth(),
+     fecha.getUTCDate()
+     );
+    return  diferenciaFechas<= fechaActual;
+}
